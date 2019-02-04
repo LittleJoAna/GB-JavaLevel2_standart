9da31a23e2cccad03227d1d4e45e2cbd09b3adb9
Anastasiya <littlejonah00@gmail.com>
2019-02-04 12:40:18 +0300

    Домашнее задание по лекции 4

---
+++ JavaFX_Fis/src/sample/Controller.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+    public void clientButtonClick(ActionEvent actionEvent) {
+        textArea.appendText(textField.getText() + "\n");
+        textField.clear();
+        textField.requestFocus();
+    }
+}
